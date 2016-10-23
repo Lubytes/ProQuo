743be69449166d0fe8979828ab2237181193b1b9
Raphael <raphael@celraph.com>
2016-10-23 11:44:16 -0300

    Fixing windows bugs
    Getting set up on a windows machines and I had some little problems, so
    I fixed them.

--- Gemfile
+++ Gemfile
@@ -16,10 +16,15 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
-gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.10', require: false
+if Bundler::WINDOWS
+  gem 'bcrypt-ruby', '~> 3.0.0', require: false
+else
+  gem 'bcrypt', '~> 3.1.10', require: false
+end
 
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
+# Windows fix
+gem 'wdm', '>= 0.1.0' if Gem.win_platform?
 
 # Use angularJS
 gem 'angularjs-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023032642) do
+ActiveRecord::Schema.define(version: 20161023143734) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
+    t.string   "username",      null: false
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.string   "password"
-    t.string   "password_hash"
+    t.string   "password_hash", null: false
   end
 
 end
