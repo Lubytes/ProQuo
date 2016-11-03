88d1641d4efcaa2c98e5090e890c6ada647b3cdc
Brandon <dood0@live.ca>
2016-11-03 19:22:21 -0300

    Login & Sign Up Button
    Created a button for logging in or signing up. Some functionality has
    been implemented.

--- app/assets/javascripts/app/proquo.js
+++ app/assets/javascripts/app/proquo.js
@@ -6,3 +6,45 @@ app.controller('MainCtrl', ['$scope',
         $scope.promo = "The most viewed trades this month.";
 
     }]);
+
+app.controller('LoginCtrl', function DemoCtrl($mdDialog) {
+        var originatorEv;
+
+        this.openMenu = function($mdOpenMenu, ev) {
+          originatorEv = ev;
+          $mdOpenMenu(ev);
+        };
+
+        this.notificationsEnabled = true;
+        this.toggleNotifications = function() {
+          this.notificationsEnabled = !this.notificationsEnabled;
+        };
+
+        this.login = function() {
+          $mdDialog.show(
+            $mdDialog.alert()
+              .targetEvent(originatorEv)
+              .clickOutsideToClose(true)
+              .parent('body')
+              .title('Login Below:')
+              .textContent('Future form goes here.')
+              .ok('Login')
+          );
+
+          originatorEv = null;
+        };
+
+        this.signup = function() {
+            $mdDialog.show(
+              $mdDialog.alert()
+                .targetEvent(originatorEv)
+                .clickOutsideToClose(true)
+                .parent('body')
+                .title('Redirect')
+                .textContent('This will redirect to a sign up page')
+                .ok('buttons and stuff')
+            );
+
+            originatorEv = null;
+        };
+      });
