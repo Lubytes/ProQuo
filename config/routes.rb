9c0438df5c492024ef1e71c425bf35284c6b9aab
RocGod <Roc.Wang@dal.ca>
2016-11-19 16:57:11 -0400

    Link the Home button to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  resources :props
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :props
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/register', to: 'user#new'
-  post '/register', to: 'user#create'
-  get '/users/:username', to: 'user#show', as: 'profile'
+    get '/register', to: 'user#new'
+    post '/register', to: 'user#create'
+    get '/users/:username', to: 'user#show', as: 'profile'
 
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  get    '/logout',  to: 'sessions#destroy'
-  get    '/about',   to: 'application#about'
-  
-  root to: 'application#index'
+    get    '/login',   to: 'sessions#new'
+    post   '/login',   to: 'sessions#create'
+    get    '/logout',  to: 'sessions#destroy'
+    get    '/about',   to: 'application#about'
+
+    root to: 'application#index'
 end
