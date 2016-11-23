edcf14ed6791efce648dd16b71357e6abc8954df
RocGod <Roc.Wang@dal.ca>
2016-11-23 15:29:26 -0400

    Add error page check
    1. error_controller.rb created
    2. add link to 404.html in route,rb
    3. Successfully go to 404 when the page is not found

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  resources :props
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :props
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/register', to: 'user#new'
-  post '/register', to: 'user#create'
-  get '/users/:username', to: 'user#show', as: 'profile'
+    get '/register', to: 'user#new'
+    post '/register', to: 'user#create'
+    get '/users/:username', to: 'user#show', as: 'profile'
 
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  get    '/logout',  to: 'sessions#destroy'
-  get    '/about',   to: 'application#about'
+    get    '/login',   to: 'sessions#new'
+    post   '/login',   to: 'sessions#create'
+    get    '/logout',  to: 'sessions#destroy'
+    get    '/about',   to: 'application#about'
 
-  root to: 'application#index'
+    root to: 'application#index'
+    match '*a', :to => 'errors#routing', via: :get
 end
