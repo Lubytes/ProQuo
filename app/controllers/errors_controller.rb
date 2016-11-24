417149e9aac86218d52976664a04accfe7044f1c
RocGod <Roc.Wang@dal.ca>
2016-11-24 10:17:04 -0400

    Link the error page with general view
    Link the header to 404 page, but the style should be designed better.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,5 +1,5 @@
 class ErrorsController < ApplicationController
   def routing
-   render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
+   render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => true
   end
 end
