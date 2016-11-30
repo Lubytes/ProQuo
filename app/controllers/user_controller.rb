4f52dac1695433c092f764a6a8b9afc094e18c08
Myst <tormyst@github.com>
2016-11-30 18:15:32 -0400

    Backend changes to allow for user profiles to be edited
    Important, updating the old version of the user won’t work, so a new
    seeded value was added.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,10 @@
 class UserController < ApplicationController
+    before_action :set_user, only: [:show, :update]
     include ApplicationHelper
 
     # User profile.
     # TODO get list of props owned by user ready.
     def show
-        @user = User.find_by_username(params[:username])
         if @user.nil?
             render 'no_user'
         end
@@ -31,7 +31,33 @@ class UserController < ApplicationController
             redirect_to :back
         end
     end
-  def user_params
-    params.permit(:username, :full_name, :email, :password, :password_confirmation, :description, :avatar)
-  end
+
+    def update
+        @user.assign_attributes user_update_params
+        if @user.save
+            redirect_to profile_path, username: @user.username, notice: 'Your user successfully updated.'
+        else
+            if @user.errors.full_messages.any?
+                @user.errors.full_messages.each do |error_message|
+                    addToErrors(:editError, error_message)
+                end
+            end
+            redirect_to profile_path, username: @user.username, notice: 'Something went wrong, your user was not updated.'
+        end
+    end
+
+    private
+
+    def set_user
+        @user = User.find_by_username(params[:username])
+    end
+
+    def user_params
+        params.permit(:username, :full_name, :email, :password, :password_confirmation, :description, :avatar)
+    end
+
+    def user_update_params
+        params.permit(:password, :password_confirmation)
+    end
+
 end
